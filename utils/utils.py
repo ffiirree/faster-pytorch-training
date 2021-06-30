425a6e298db6e827cef173aec6c71168ae535804
ffiirree <ice_qi@163.com>
2021-06-30 17:53:49 +0800

    ImageNet Training

--- utils/utils.py
+++ utils/utils.py
@@ -3,8 +3,10 @@ from json import dumps
 import torch
 import torchvision
 import platform
+import numpy as np
+import random
 
-__all__ = ['Benchmark', 'env_info']
+__all__ = ['Benchmark', 'env_info', 'manual_seed']
 
 class Benchmark:
     def __init__(self) -> None:
@@ -45,3 +47,20 @@ def env_info(json: bool = False):
     }
 
     return kvs if not json else dumps(kvs, indent=4, separators=(',', ':'))
+
+
+def manual_seed(seed: int = 0):
+    r"""
+        https://pytorch.org/docs/stable/notes/randomness.html
+        https://discuss.pytorch.org/t/random-seed-initialization/7854/20
+    """
+    # numpy
+    np.random.seed(seed)
+    # Python
+    random.seed(seed)
+    # pytorch
+    torch.manual_seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+    torch.set_printoptions(precision=10)
